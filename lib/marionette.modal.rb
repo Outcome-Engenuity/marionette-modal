85aa36d464ef662fdccad3f595c221c10b797582
Jared Smith <jcsmith1859@gmail.com>
2013-12-05 13:01:32 -0600

    forgot to change the method being used.

--- lib/marionette.modal.rb
+++ lib/marionette.modal.rb
@@ -2,6 +2,6 @@ require "marionette.modal/version"
 
 module MarionetteModal
   module Assets
-    MARIONETTE_MODAL_COFFEE = File.open(File.join(File.dirname('..', __FILE__), 'src', 'marionette.modal.coffee'), 'r').read
+    MARIONETTE_MODAL_COFFEE = File.open(File.join(File.expand_path('..', __FILE__), 'src', 'marionette.modal.coffee'), 'r').read
   end
 end
