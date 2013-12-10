5924f6e0a1b3312f6970380bffaa0004966ddf41
Jared Smith <jcsmith1859@gmail.com>
2013-12-10 10:44:42 -0600

    Added functionality to the gem for the purposes of embedding it in the rails gem.

--- lib/marionette.modal.rb
+++ lib/marionette.modal.rb
@@ -2,6 +2,21 @@ require "marionette.modal/version"
 
 module MarionetteModal
   module Assets
-    MARIONETTE_MODAL_COFFEE = File.open(File.join(File.expand_path('..', __FILE__), 'src', 'marionette.modal.coffee'), 'r').read
+    module Javascripts
+      module Marionette
+        FILE = File.open( File.expand_path('../marionette.modal.js', File.dirname(__FILE__) ), 'r').read
+        REGION_FILE = File.open( File.expand_path('../backbone.marionette.modals.js', File.dirname(__FILE__) ), 'r').read
+      end
+      module Backbone
+        FILE = File.open( File.expand_path('../backbone.modal.js', File.dirname(__FILE__) ), 'r').read
+      end
+      module Bundle
+        FILE = File.open( File.expand_path('../marionette.modal-bundled.js', File.dirname(__FILE__) ), 'r').read
+      end
+    end
+    module Stylesheets
+      CSS = File.open( File.expand_path('../marionette.modal.css', File.dirname(__FILE__) ), 'r').read
+      THEME_CSS = File.open( File.expand_path('../marionette.modal.theme.css', File.dirname(__FILE__) ), 'r').read
+    end
   end
 end
--- lib/marionette.modal/version.rb
+++ lib/marionette.modal/version.rb
@@ -1,3 +1,3 @@
 module MarionetteModal
-  VERSION = "1.0.0.4"
+  VERSION = "1.0.0.5"
 end
